af87d5d6afa1e4b7c2afd8cf27111a0f8b70d995
Lipzs <61035595+Lipzs@users.noreply.github.com>
2020-06-03 23:26:30 -0300

    terceiro dia

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -5,7 +5,7 @@ function populateUFs() {
     .then(res => res.json())
     .then(states => {
       for (state of states) {
-        ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
       }
     });
 }
@@ -23,11 +23,16 @@ function getCities(event) {
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
+
+  citySelect.innerHTML = "<option value>Selecione a Cidade</option>";
+  citySelect.disabled = true;
+
+
   fetch(url)
     .then(res => res.json())
     .then(cities => {
       for (city of cities) {
-        citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
       }
 
       citySelect.disabled = false;
@@ -37,3 +42,42 @@ function getCities(event) {
 document
   .querySelector("select[name=uf]")
   .addEventListener("change", getCities);
+
+//----------------itens de coleta-------------------//
+
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+for (const item of itemsToCollect) {
+  item.addEventListener("click", handleSelectedItem);
+};
+
+const collectedItems = document.querySelector("input[name=items]");
+
+let selectedItems = [];
+
+function handleSelectedItem(event) {
+  const itemLi = event.target;
+
+  itemLi.classList.toggle("selected");
+
+  const itemId = itemLi.dataset.id;
+
+  const alreadySelected = selectedItems.findIndex(function (item) {
+    const itemFound = item == itemId;
+    return itemFound;
+  });
+
+
+  if (alreadySelected >= 0) {
+    const filteredItem = selectedItems.filter(item => {
+      const itemIsDiferent = item != itemId;
+      return itemIsDiferent;
+    });
+
+    selectedItems = filteredItem;
+  } else {
+    selectedItems.push(itemId);
+  };
+
+  collectedItems.value = selectedItems;
+};
